0d36a76160415f412f63b1d1536b71d057560d71
Akunov4594 <frontdevkg@gmail.com>
2023-09-13 13:03:18 +0600

    start info

---
+++ src/pages/ProductInfo/ProductInfoPage.jsx
@@ -0,0 +1,16 @@
+import cls from './ProductInfoPage.module.scss'
+
+const ProductInfoPage = () => {
+  return (
+    <div className={cls.productWrap}>
+      <div className={cls.product}>
+        <div>
+          <img src="https://avatars.mds.yandex.net/get-images-cbir/3927539/QjfaojOnQg6ifr-sFOcqWA8271/ocr" alt="" />
+        </div>
+        <div></div>
+      </div>
+    </div>
+  )
+}
+
+export default ProductInfoPage
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom"
 import Layout from "../Layout/Layout"
 import HomePage from "../pages/HomePage/HomePage"
+import ProductInfoPage from "../pages/ProductInfo/ProductInfoPage"
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
+        <Route path="product/:id" element={<ProductInfoPage />} />
       </Route>
     </Routes>
   )
