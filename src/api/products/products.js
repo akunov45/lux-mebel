558d526fd54336c050fa7a2e627930ab41140237
Akunov4594 <86850933+akunov45@users.noreply.github.com>
2023-12-16 13:38:19 +0600

    update api url and add new dependency strapi sdk, fixed mobile styles some pages

--- src/api/products/products.js
+++ src/api/products/products.js
@@ -3,11 +3,13 @@ import {strapi} from "../strapi/api"
 const NS = "products"
 
 const productsApi = {
+  
   findProducts() {
     return strapi.find(NS, {
       populate: "image"
     })
   },
+  
   findOneProduct(id) {
     return strapi.findOne(NS, id, {
       populate: "image"
--- src/store/singleProductSlice/singleProductSlice.js
+++ src/store/singleProductSlice/singleProductSlice.js
@@ -1,11 +1,10 @@
 import {createAsyncThunk, createSlice} from '@reduxjs/toolkit'
 import productsApi from "../../api/products/products.js";
-// import axios from 'axios'
 
 export const getSingleProduct = createAsyncThunk('singleProduct/getSingleProduct', async (id) => {
-  //const {data}=await axios('https://651536f3dc3282a6a3ce04f0.mockapi.io/furniture/'+ id)
   return await productsApi.findOneProduct(id)
 })
+
 const singleProductSlice = createSlice({
   name: 'singleProduct',
   initialState: {
@@ -13,7 +12,6 @@ const singleProductSlice = createSlice({
     isLoading: false,
     error: ''
   },
-  reducers: {},
   extraReducers: (builder) => {
     builder
       .addCase(getSingleProduct.pending, (state) => {
